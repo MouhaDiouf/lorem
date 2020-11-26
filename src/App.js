071445d8d370dd4f22df570c98e391ec5d9a05fe
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-11-26 15:47:52 +0000

    Create the handleSubmit function and the form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className="lorem" onSubmit={handleSubmit}></form>
+    </section>
   );
 }
 
